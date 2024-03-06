a5a55a52cf3c30680f84bdf01621d587cf1075aa
poky <pokyria@gmail.com>
2024-03-07 12:44:43 +1300

    deploy finally working

--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-const rootElement = document.getElementById('root');
-
-const root = createRoot(rootElement);
-
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
- <StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
- </StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
